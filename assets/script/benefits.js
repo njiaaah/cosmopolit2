ebcf66250ed76ffc6c68eaa9a858c4a370e53477
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2024-04-13 19:02:08 +0300

    style2 ARC

---
+++ assets/script/benefits.js
@@ -0,0 +1,13 @@
+const benefitsSwiper = new Swiper('.benefits__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    breakpoints: {
+        1: {
+            spaceBetween: 16
+        },
+        720: {
+            spaceBetween: 32
+        }
+    }
+      
+  });
---
+++ assets/script/promo.js
@@ -0,0 +1,18 @@
+const promoSwiper = new Swiper('.promo__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    breakpoints: {
+        1: {
+            spaceBetween: 12
+        },
+        600: {
+            spaceBetween: 32
+        }
+    },
+    pagination: {
+        el: '.gallery__swiper-pagination_promo',
+        type: 'bullets',
+        clickable: true
+      },
+      
+  });
